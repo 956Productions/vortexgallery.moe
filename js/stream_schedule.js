23779e5ac818d3f3312c0112c0b61a97f22843aa
Kevin Higgins <shiburizu312@gmail.com>
2023-02-06 02:48:36 -0600

    schedule

--- js/stream_schedule.js
+++ js/stream_schedule.js
@@ -9,18 +9,18 @@ $(function() {
             </figure>
         </div>
         <div class="column">
-            <h1 class="exo title is-1 mb-4 shadowed-text">$name ($region)</h1>
+            <h1 class="exo title is-1 mb-4 shadowed-text">$name ($region)$phase</h1>
             <span class="icon-text is-size-3">
                 <span class="icon has-text-white">
-                    <i class="fas fa-home"></i>
+                    <i class="fas fa-tv"></i>
                 </span>
-                <span><h1 class="ml-2 exo has-text-white shadowed-text">/$channel</h1></span>
+                <span><h1 class="ml-2 exo has-text-white shadowed-text">$channel</h1></span>
             </span>
             <span class="ml-4 icon-text is-size-3">
                 <span class="icon has-text-white">
-                    <i class="fas fa-home"></i>
+                    <i class="fas fa-clock"></i>
                 </span>
-                <span><h1 class="ml-2 exo has-text-white shadowed-text">$time</h1></span>
+                <span><h1 class="ml-2 exo has-text-white shadowed-text">$time - $count</h1></span>
             </span>
         </div>
     </div>
@@ -32,63 +32,51 @@ $(function() {
 <div class="box stream-box has-text-centered" style="height: 100%;">
     <div class="columns is-vcentered">
     <div class="column">
-    <div class="columns is-gapless is-vcentered" style="height: 12vh;">
+    <div class="columns is-gapless is-vcentered p-0 m-0" style="height: 12vh;">
         <div class="column">
             <figure class="image is-hidden gameLogo is-flex is-justify-content-center">
                 <img style="image-rendering: optimizequality; max-height: 12vh; width: auto;" src="$logo">
             </figure>
         </div>
         <div class="column">
-            <h1 class="exo title is-3 mb-2 shadowed-text">$region</h1>
+            <h1 class="exo is-size-4 has-text-white shadowed-text">$region$phase</h1>
             <h1 class="exo is-size-4 has-text-white shadowed-text">$time</h1>
+            <h1 class="exo is-size-4 has-text-white shadowed-text">$count</h1>
         </div>
     </div>
-    <span class="icon-text is-size-3">
-        <span class="icon has-text-white">
-            <i class="fas fa-home"></i>
-        </span>
-        <span><h1 class="ml-2 exo has-text-white shadowed-text">/$channel</h1></span>
-    </span>
+    <h1 class="ml-2 exo is-size-4 has-text-white shadowed-text">$channel</h1>
     </div>
     </div>
 </div>
 </div>`
 
-later_scroll_event_template = 
+    later_scroll_event_template = 
 `<div class="block page-2 is-hidden">
 <div class="box stream-box has-text-centered" style="height: 100%;">
     <div class="columns is-vcentered">
     <div class="column">
-    <div class="columns is-gapless is-vcentered" style="height: 12vh;">
+    <div class="columns is-gapless is-vcentered p-0 m-0" style="height: 12vh;">
         <div class="column">
             <figure class="image is-hidden gameLogo is-flex is-justify-content-center">
                 <img style="image-rendering: optimizequality; max-height: 12vh; width: auto;" src="$logo">
             </figure>
         </div>
         <div class="column">
-            <h1 class="exo title is-3 mb-2 shadowed-text">$region</h1>
+            <h1 class="exo is-size-4 has-text-white shadowed-text">$region$phase</h1>
             <h1 class="exo is-size-4 has-text-white shadowed-text">$time</h1>
+            <h1 class="exo is-size-4 has-text-white shadowed-text">$count</h1>
         </div>
     </div>
-    <span class="icon-text is-size-3">
-        <span class="icon has-text-white">
-            <i class="fas fa-home"></i>
-        </span>
-        <span><h1 class="ml-2 exo has-text-white shadowed-text">/$channel</h1></span>
-    </span>
+    <h1 class="ml-2 exo is-size-4 has-text-white shadowed-text">$channel</h1>
     </div>
     </div>
 </div>
 </div>`
 
-    $.getJSON ('stream_schedule.json', function ( json ) {
+    $.getJSON ('https://ninefivesix-aigis.onrender.com/', function ( json ) {
         nextGames = Object.values(json).slice(0,3)
         laterGames = Object.values(json).slice(3,7)
         laterScrollGames = Object.values(json).slice(7,11)
-        console.log(nextGames)
-        console.log(laterGames)
-        console.log(laterScrollGames)
-
 
         for (var i = 2; i > -1; i--) {
             event_element = next_event_template
@@ -96,6 +84,9 @@ later_scroll_event_template =
             event_element = event_element.replace("$channel",nextGames[i].channel)
             event_element = event_element.replace("$logo",nextGames[i].logo)
             event_element = event_element.replace("$region",nextGames[i].region)
+            event_element = event_element.replace("$phase",nextGames[i].phase)
+            event_element = event_element.replace("$time",nextGames[i].date + " " + nextGames[i].time + " " + nextGames[i].tz)
+            event_element = event_element.replace("$count",nextGames[i].delta)
             $('.schedule-now-header').after(event_element)
         }
 
@@ -105,6 +96,9 @@ later_scroll_event_template =
             event_element = event_element.replace("$channel",laterGames[i].channel)
             event_element = event_element.replace("$logo",laterGames[i].logo)
             event_element = event_element.replace("$region",laterGames[i].region)
+            event_element = event_element.replace("$phase",laterGames[i].phase)
+            event_element = event_element.replace("$time",laterGames[i].date)
+            event_element = event_element.replace("$count",laterGames[i].time + " " + laterGames[i].tz)
             $('.schedule-later-header').after(event_element)
         }
 
@@ -114,6 +108,9 @@ later_scroll_event_template =
             event_element = event_element.replace("$channel",laterScrollGames[i].channel)
             event_element = event_element.replace("$logo",laterScrollGames[i].logo)
             event_element = event_element.replace("$region",laterScrollGames[i].region)
+            event_element = event_element.replace("$phase",laterScrollGames[i].phase)
+            event_element = event_element.replace("$time",laterScrollGames[i].date)
+            event_element = event_element.replace("$count",laterScrollGames[i].time + " " + laterScrollGames[i].tz)
             $('.schedule-later-header').after(event_element)
         }
 
@@ -149,6 +146,6 @@ later_scroll_event_template =
             $('.container-fluid').removeClass('is-hidden')
             $('.container-fluid').addClass('fade-in-stream')
         })
-    },200)
+    },1500)
 })
   
